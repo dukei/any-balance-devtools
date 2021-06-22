78aa2dd867532a4bd9c76548901ce2b9d3262367
Dmitry Kochin <dco@mail.ru>
2021-06-22 10:03:31 +0300

    Процедура сборки

--- postbuild.bat
+++ postbuild.bat
@@ -1,10 +1,11 @@
-﻿mkdir "./dist/google-closure-compiler-java"
-mkdir "./dist/chromium"
-mkdir "./dist/google-closure-compiler-java"
-mkdir "./dist/google-closure-compiler-windows"
-mkdir "./dist/profiles"
-mkdir "./dist/config"
-copy ".\build\app\config\_config_production.js" ".\dist\config\config_production.js" /y
-xcopy "./node_modules/puppeteer/.local-chromium" "./dist/chromium" /s /e /y
-copy ".\node_modules\google-closure-compiler-java\compiler.jar" ".\dist\google-closure-compiler-java\" /y
-copy ".\node_modules\google-closure-compiler-windows\compiler.exe" ".\dist\google-closure-compiler-windows\" /y
+﻿mkdir "./abd/google-closure-compiler-java"
+mkdir "./abd/chromium"
+mkdir "./abd/google-closure-compiler-java"
+mkdir "./abd/google-closure-compiler-windows"
+mkdir "./abd/profiles"
+mkdir "./abd/config"
+copy ".\build\app\config\_config_production.js" ".\abd\config\config_production.js" /y
+xcopy "./node_modules/puppeteer/.local-chromium" "./abd/chromium" /s /e /y
+copy ".\node_modules\google-closure-compiler-java\compiler.jar" ".\abd\google-closure-compiler-java\" /y
+copy ".\node_modules\google-closure-compiler-windows\compiler.exe" ".\abd\google-closure-compiler-windows\" /y
+ren ".\abd\any-balance-devtools.exe" "abd.exe"
